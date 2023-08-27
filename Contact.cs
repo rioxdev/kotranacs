5ee5ac97d733f6a3093a69b573ceb0ace2f8e155
Henrio Randriasoa <rioxdev@gmail.com>
2023-08-27 13:57:55 +0200

    init

---
+++ Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace kotranacs
+{
+    public record Contact(
+        [property: JsonPropertyName("first")]
+        string FirstName,
+        [property: JsonPropertyName("last")]
+        string LastName,
+        string Email)
+    {
+        public string FullName => $"{FirstName} {LastName}";
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+
+namespace kotranacs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kotrana cs");
+
+            var alexis = new Contact("Alexis", "Sanchez", "asanchez@gmail.com");
+            Console.WriteLine(alexis);
+
+            var (_, _, alexisEmail) = alexis;
+            Console.WriteLine(alexisEmail);
+
+            var json = JsonSerializer.Serialize(alexis, options: new JsonSerializerOptions() { WriteIndented = true});
+            Console.WriteLine(json);
+
+        }
+    }
+}
